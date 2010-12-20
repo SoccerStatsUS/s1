bc504194b68f838a013757254e5bbdf6242b6eaf
Chris Edgemon <chris@reyna.(none)>
2010-12-20 21:09:23 +0000

    Add about page, news urls, schedules urls

--- urls.py
+++ urls.py
@@ -6,12 +6,15 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
                        url(r'^$', include('soccer.main.urls')),
+                       url(r'^about/$', 'soccer.main.views.about', name='about'),
                        url(r'^p/', include('soccer.players.urls')),
                        url(r'^drafts/', include('soccer.drafts.urls')),
+                       url(r'^news/', include('soccer.feeds.urls')),
                        url(r'^places/', include('soccer.places.urls')),
                        url(r'^international/', include('soccer.international.urls')),                       
                        url(r'^leagues/', include('soccer.leagues.urls')),
                        url(r'^salaries/', include('soccer.salaries.urls')),
+                       url(r'^schedules/', include('soccer.lineups.urls')),
                        url(r'^stats/', include('soccer.stats.urls')),
                        url(r'^teams/', include('soccer.teams.urls')),
 
