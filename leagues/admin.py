8025715cbde8243c7417686fbdeb84adb4c76a55
Chris Edgemon <chrisedgemon@gmail.com>
2011-09-10 16:04:19 -0400

    Add league admin.

--- leagues/admin.py
+++ leagues/admin.py
@@ -1,5 +1,10 @@
 from soccer.leagues.models import League
 from django.contrib import admin
 
-admin.site.register(League)
+class LeagueAdmin(admin.ModelAdmin):    
+    list_display = ("name", "confederation")
+    list_filter = ("confederation",)
+
+
+admin.site.register(League, LeagueAdmin)
 
